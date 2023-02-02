28126096f05dd3a2e13d228f399ac9a38e84c682
Navpreet singh <navpreetsingh41548@gmail.com>
2023-02-02 07:35:11 -0800

    Journey Section Smooth

--- public/js/animate.js
+++ public/js/animate.js
@@ -159,66 +159,102 @@ if (gsap !== undefined && ScrollTrigger !== undefined) {
   // }
   // !
 
-  var controller = new ScrollMagic.Controller();
-  var sections = document.querySelectorAll(".JourneySection_slide__J88nk");
-  var tl2 = new TimelineMax({
-    onUpdate: onUpdate,
-    onComplete: onComplete,
-    onReverseComplete: onReverseComplete,
-  });
-  var offset = window.innerHeight;
-  tl2.to(sections[0], 1, { xPercent: -100, ease: Linear.easeNone }, "+=1");
-  tl2.to(sections[1], 1, { xPercent: 100, ease: Linear.easeNone }, "+=1");
-  // tl.to(sections[3], 1, { xPercent: 100, ease: Linear.easeNone }, "+=1");
-  // tl.to(sections[4], 1, { yPercent: 100, ease: Linear.easeNone }, "+=1");
-  // tl.to(sections[5], 1, { xPercent: -100, ease: Linear.easeNone }, "+=1");
-  // tl.to(sections[6], 1, { yPercent: -100, ease: Linear.easeNone }, "+=1");
-  // tl.to(sections[7], 1, { xPercent: -100, ease: Linear.easeNone }, "+=1");
-  // tl.to(sections[8], 1, { yPercent: 100, ease: Linear.easeNone }, "+=1");
-  // tl.to(sections[9], 1, { xPercent: -100, ease: Linear.easeNone }, "+=1");
-  // tl.to(sections[10], 1, { xPercent: 100, ease: Linear.easeNone }, "+=1");
-  // tl.to(sections[11], 1, { xPercent: -100, ease: Linear.easeNone }, "+=1");
+  // !
+  // var controller = new ScrollMagic.Controller();
+  // var sections = document.querySelectorAll(".JourneySection_slide__J88nk");
+  // var tl2 = new TimelineMax({
+  //   onUpdate: onUpdate,
+  //   onComplete: onComplete,
+  //   onReverseComplete: onReverseComplete,
+  // });
+  // var offset = window.innerHeight;
+  // tl2.to(sections[0], 1, { xPercent: -100, ease: Linear.easeNone }, "+=1");
+  // tl2.to(sections[1], 1, { xPercent: 100, ease: Linear.easeNone }, "+=1");
+  // // tl.to(sections[3], 1, { xPercent: 100, ease: Linear.easeNone }, "+=1");
+  // // tl.to(sections[4], 1, { yPercent: 100, ease: Linear.easeNone }, "+=1");
+  // // tl.to(sections[5], 1, { xPercent: -100, ease: Linear.easeNone }, "+=1");
+  // // tl.to(sections[6], 1, { yPercent: -100, ease: Linear.easeNone }, "+=1");
+  // // tl.to(sections[7], 1, { xPercent: -100, ease: Linear.easeNone }, "+=1");
+  // // tl.to(sections[8], 1, { yPercent: 100, ease: Linear.easeNone }, "+=1");
+  // // tl.to(sections[9], 1, { xPercent: -100, ease: Linear.easeNone }, "+=1");
+  // // tl.to(sections[10], 1, { xPercent: 100, ease: Linear.easeNone }, "+=1");
+  // // tl.to(sections[11], 1, { xPercent: -100, ease: Linear.easeNone }, "+=1");
 
-  new ScrollMagic.Scene({
-    triggerElement: "#journey",
-    triggerHook: 0,
-    duration: "500%",
-  })
-    .setPin("#journey")
-    .setTween(tl2)
+  // new ScrollMagic.Scene({
+  //   triggerElement: "#journey",
+  //   triggerHook: 0,
+  //   duration: "500%",
+  // })
+  //   .setPin("#journey")
+  //   .setTween(tl2)
 
-    .addTo(controller);
+  //   .addTo(controller);
 
-  for (let i = 0; i < sections.length; i++) {
-    const element = sections[i];
-    var tl2 = new TimelineMax();
+  // for (let i = 0; i < sections.length; i++) {
+  //   const element = sections[i];
+  //   var tl2 = new TimelineMax();
 
-    new ScrollMagic.Scene({
-      triggerElement: "#journey",
-      triggerHook: 0,
-      offset: i * offset,
-    })
-      .setTween(tl2)
-      .addTo(controller);
-  }
+  //   new ScrollMagic.Scene({
+  //     triggerElement: "#journey",
+  //     triggerHook: 0,
+  //     offset: i * offset,
+  //   })
+  //     .setTween(tl2)
+  //     .addTo(controller);
+  // }
 
-  const journey = document.getElementById("journey");
+  // const journey = document.getElementById("journey");
 
-  function onUpdate(self) {
-    if (journey.getBoundingClientRect().top <= 40) {
-      document.getElementById("nav").classList.add("transformNav");
-    } else {
-      document.getElementById("nav").classList.remove("transformNav");
-    }
-  }
-  function onComplete() {
-    document.getElementById("nav").classList.remove("transformNav");
-  }
-  function onReverseComplete() {
-    document.getElementById("nav").classList.remove("transformNav");
-  }
+  // function onUpdate(self) {
+  //   if (journey.getBoundingClientRect().top <= 40) {
+  //     document.getElementById("nav").classList.add("transformNav");
+  //   } else {
+  //     document.getElementById("nav").classList.remove("transformNav");
+  //   }
+  // }
+  // function onComplete() {
+  //   document.getElementById("nav").classList.remove("transformNav");
+  // }
+  // function onReverseComplete() {
+  //   document.getElementById("nav").classList.remove("transformNav");
+  // }
+
+  // !
 
   // $(".JourneySection_slide__J88nk").each(function (i) {});
+
+  const tl2 = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#journey",
+      start: "top top",
+      // end: "bottom top",
+      end: "+=" + window.innerHeight * 2,
+      pin: "#journey",
+      markers: true,
+      onUpdate: (self) => {
+        if (self.progress > 0 && self.progress < 0.25) {
+          document.getElementById("slide1").classList.remove("transformSlide");
+        } else if (self.progress > 0.25 && self.progress < 0.5) {
+          document.getElementById("slide1").classList.add("transformSlide");
+          document.getElementById("slide2").classList.remove("transformSlide2");
+        } else if (self.progress > 0.5 && self.progress < 0.75) {
+          document.getElementById("slide2").classList.add("transformSlide2");
+          document.getElementById("slide3").classList.remove("transformSlide3");
+        } else if (self.progress > 0.75 && self.progress < 1) {
+          document.getElementById("slide3").classList.add("transformSlide3");
+        }
+
+        if (self.progress > 0.99) {
+          // document.getElementById("nav").classList.remove("sticky");
+          document.getElementById("nav").classList.remove("transformNav");
+        } else {
+          document.getElementById("nav").classList.add("transformNav");
+        }
+      },
+      onLeaveBack: () =>
+        document.getElementById("nav").classList.remove("transformNav"),
+    },
+  });
 }
 
 const tl3 = gsap.timeline({
